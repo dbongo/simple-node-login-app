abd98eac0789ef47e2f16e8a404c544a6b0f2513
Rob Foweraker <djinnster@gmail.com>
2014-08-27 15:03:20 +0200

    removed role.js and updated package.json

--- app/models/role.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var roles = 'admin user'.split(' ');
-var Role = new Schema({
-    role: { type: String, enum: roles }
-});
-
-module.exports = mongoose.model('Role', Role);
